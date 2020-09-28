46871f192438a6179f39476446aa8af0f7438a7b
Parwan Koshal <parwan.koshal@hangarww.com>
2020-09-28 22:25:00 +0400

    create file structure

---
+++ src/components/Message/Message.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Message.css';
+
+function Message() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Message
---
+++ src/components/MessageBody/MessageBody.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MessageBody.css'
+
+function MessageBody() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MessageBody
