34417e22b632421646593ff6e78fd55f90aba284
Parwan Koshal <parwan.koshal@hangarww.com>
2020-09-29 09:18:06 +0400

    [facebook-messenger] add user profile menu

--- src/components/ProfileMenu/ProfileMenu.js
+++ src/components/ProfileMenu/ProfileMenu.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React from 'react';
 import './ProfileMenu.css';
+import { data } from './ProfileMenu.json';
 
-function ProfileMenu() {
+function ProfileMenu({ className }) {
     return (
-        <div>
-            
-        </div>
-    )
+        <div className={`profileMenu ${className}`} contentEditable={true}>
+            <div className="profileMenu__row">
+                <div className="profileMenu__arrow" />
+
+                <ul className="profileMenu__list">
+                    {
+                        data.map((lists, index) => (
+                            <li key={index}>
+                                <ul>
+                                    {
+                                        lists.map((list, key) => (
+                                            <li key={key}>
+                                                <a href={list.url !== null ? list.url : "#"}>
+                                                    <div className="icon">
+                                                        {list.icon !== "" && (<i className={list.icon}> </i>)}
+                                                    </div>
+
+                                                    <div className="profileMenu_desc">
+                                                        <h5>{list.title}</h5>
+                                                        {list.subtitle !== "" && (<small>{list.subtitle}</small>)}
+                                                    </div>
+                                                </a>
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
+                            </li>
+                        ))
+                    }
+                </ul >
+
+            </div>
+        </div >
+    );
 }
 
-export default ProfileMenu
+export default ProfileMenu;
