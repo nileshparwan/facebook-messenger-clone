0b56d8cc4b0a9e00341526a9471edd0559d329dd
Parwan Koshal <parwan.koshal@hangarww.com>
2020-09-29 09:19:03 +0400

    [facebook-messenger] add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Profile from '../Profile/Profile';
+import { headerButtons } from './Header.json';
 import './Header.css';
 
 function Header() {
     return (
-        <div>
-            
-        </div>
-    )
+        <header className="header">
+            <div className="header__row">
+                <Profile />
+                <div className="header__btns">
+                    {
+                        headerButtons.reverse().map((icon, index) => (
+                            <button key={index} aria-label={icon.split("-")[1]}>
+                                <i className={`${icon}`}></i>
+                            </button>
+                        ))
+                    }
+                </div>
+            </div>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
