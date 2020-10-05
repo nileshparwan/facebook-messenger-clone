5b31d378ec8d100dcf2f74d589087685f586c6df
Parwan Koshal <parwan.koshal@hangarww.com>
2020-10-05 09:02:30 +0400

    [facebookMessenger] update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import Profile from '../Profile/Profile';
 import { headerButtons } from './Header.json';
+import ProfileMenu from '../ProfileMenu/ProfileMenu';
+import FeatureOptions from '../FeatureOptions/FeatureOptions';
 import './Header.css';
 
+
 function Header() {
+    const headerRef = React.createRef();
+    const ProfileHandler = (event) => {
+        // event.preventDefault();
+        const headerElem = headerRef?.current;
+        const profileMenu = headerElem.querySelector('.header__profileMenu');
+        if (!profileMenu.classList.contains('show')) return profileMenu.classList.add('show');
+        if (profileMenu.classList.contains('show')) return profileMenu.classList.remove('show');
+        return;
+    };
     return (
-        <header className="header">
-            <div className="header__row">
-                <Profile />
-                <div className="header__btns">
-                    {
-                        headerButtons.reverse().map((icon, index) => (
-                            <button key={index} aria-label={icon.split("-")[1]}>
-                                <i className={`${icon}`}></i>
-                            </button>
-                        ))
-                    }
+        <>
+            <header className="header" ref={headerRef}>
+                <ProfileMenu className="header__profileMenu" />
+                <div className="header__row">
+                    <Profile profileHandler={(e) => ProfileHandler(e)} />
+                    <FeatureOptions className="featureOptionsCustom" options={headerButtons} />
                 </div>
-            </div>
-        </header>
+            </header>
+        </>
     );
 }
 
