4f95b976c3a1cec1473dc298d414e6478c40e1e9
Parwan Koshal <parwan.koshal@hangarww.com>
2020-10-05 09:03:19 +0400

    [facebookMessenger] [app] assembling components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.css';
+import MessageBody from './components/MessageBody/MessageBody';
+import Message, { USERTYPE,POSITION } from './components/Message/Message';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-// import ProfileMenu from './components/ProfileMenu/ProfileMenu';
 // import Profile from './components/Profile/Profile';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Profile /> */}
-      {/* <ProfileMenu /> */}
-      <Header />
+      <div className="facebookMessenger">
+        {/* <Profile /> */}
+        <Header />
+        <MessageBody>
+          <Message user={USERTYPE.CURRENT} message="Twe vin vieux NeV" />
+          <Message user={USERTYPE.NEXTCURRENT} position={POSITION.LEFT} message="Non mamou p vin experiencer" />
+        </MessageBody>
+        <Footer />
+      </div>
+
     </div>
   );
 }
